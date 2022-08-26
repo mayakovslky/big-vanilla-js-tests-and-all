a87ce3c377092240b97ac49e274617dd17910946
mayakovsky <mayakovsky03@gmail.com>
2022-08-26 14:40:01 +0300

    add test sum and mult

---
+++ src/01-hello-tests/01.test.ts
@@ -0,0 +1,33 @@
+import {mult, sum} from "./01";
+
+test('sum should be correct', () => {
+    // data
+    const a = 1;
+    const b = 2;
+    const c = 3;
+
+    //action
+    const result1 = sum(a, b);
+    const result2 = sum(b, c);
+
+    //expect
+    expect (result1).toBe(3);
+    expect(result2).toBe(5);
+
+})
+
+test('multiply should be correct', () => {
+    // data
+    const a = 1;
+    const b = 2;
+    const c = 3;
+
+    //action
+    const result1 = mult(a, b);
+    const result2 = mult(b, c);
+
+    //expect
+    expect (result1).toBe(2);
+    expect(result2).toBe(6);
+
+})
