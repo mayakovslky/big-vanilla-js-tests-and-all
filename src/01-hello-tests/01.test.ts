ec5fa3eda9cd38527f27bb59d73d8deb13294e03
mayakovsky <mayakovsky03@gmail.com>
2022-08-26 15:13:35 +0300

    add test filter, map, replace

--- src/01-hello-tests/01.test.ts
+++ src/01-hello-tests/01.test.ts
@@ -43,4 +43,11 @@ test('splitting into words should be correct', () => {
     expect(result1[0]).toBe('Hello');
     expect(result1[1]).toBe('my');
     expect(result1[2]).toBe('friend');
+
+    expect(result2.length).toBe(5);
+    expect(result2[0]).toBe('JS');
+    expect(result2[1]).toBe('the');
+    expect(result2[2]).toBe('best');
+    expect(result2[3]).toBe('programming');
+    expect(result2[4]).toBe('language');
 })
